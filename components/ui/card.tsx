f92de379430252e19c60bfa4ded062a3eac2b6f6
biitassets-VSS <biit.assets@gmail.com>
2026-06-05 23:22:16 +0530

    Add UI component library

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,5 +1,4 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+﻿import React from 'react'
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -7,64 +6,34 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border border-gray-200 bg-white text-gray-950 shadow-sm",
-      className
-    )}
+    className={ounded-lg border border-gray-200 bg-white shadow-sm +""+${className || ''}+""+}
     {...props}
   />
 ))
-Card.displayName = "Card"
+Card.displayName = 'Card'
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+  <div ref={ref} className={p-6 +""+${className || ''}+""+} {...props} />
 ))
-CardHeader.displayName = "CardHeader"
+CardHeader.displayName = 'CardHeader'
 
 const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
+  HTMLHeadingElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
-    {...props}
-  />
+  <h2 ref={ref} className={	ext-2xl font-bold +""+${className || ''}+""+} {...props} />
 ))
-CardTitle.displayName = "CardTitle"
-
-const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-gray-600", className)}
-    {...props}
-  />
-))
-CardDescription.displayName = "CardDescription"
+CardTitle.displayName = 'CardTitle'
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={p-6 pt-0 +""+${className || ''}+""+} {...props} />
 ))
-CardContent.displayName = "CardContent"
+CardContent.displayName = 'CardContent'
 
-const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
-))
-CardFooter.displayName = "CardFooter"
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader, CardTitle, CardContent }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,22 +1,17 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+﻿import React from 'react'
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
-  return (
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => (
     <input
-      type={type}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
+      className={px-3 py-2 border border-gray-300 rounded +""+${className || ''}+""+}
       ref={ref}
       {...props}
     />
   )
-})
-Input.displayName = "Input"
+)
+Input.displayName = 'Input'
 
 export { Input }
