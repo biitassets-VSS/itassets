34c5ebe027a1fdba86b97e1e84ba53727d66b2ce
biitassets-VSS <biit.assets@gmail.com>
2026-06-05 20:15:02 +0530

    🧹 FINAL CLEAN: Removed tailwind.config.ts and all config files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Clean configuration for Next.js 16
-  experimental: {
-    // Future optimizations can go here
-  }
-}
-
-module.exports = nextConfig
